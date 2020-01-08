2bafea06b205f9b5b8a6e02e58250006482a0745
Meg <mmwoodfo@asu.edu>
2020-01-08 12:25:54 -0700

    Adding gift & user classes

---
+++ SecretSanta/src/SecretSanta.Business/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SecretSanta.Business
+{
+    public class User
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Collection<Gift> Gifts { get; set; }
+
+        public User(int id, string firstName, string lastName, Collection<Gift> gifts)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Gifts = gifts;
+        }
+    }
+}
