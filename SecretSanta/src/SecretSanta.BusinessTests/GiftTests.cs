342cde8025a424d418deb1f12d70d0ba2a0b70a6
Meg <mmwoodfo@asu.edu>
2020-01-10 11:52:48 -0700

    Added folders & added unit tests to SecretSanta.Business
    Added folders to better organize unit test

---
+++ SecretSanta/src/SecretSanta.BusinessTests/GiftTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.Business.Tests
+{
+    [TestClass()]
+    public class GiftTests
+    {
+        [TestMethod()]
+        public void Gift()
+        {
+            //Arrange
+            int id = 0001;
+            string title = "Test Gift";
+            string description = "This gift is a test";
+            string url = "http://gifttest.com";
+            User janeDoe = new User(0001, "Jane", "Doe", null);
+
+            //Act
+            Gift gift = new Gift(id, title, description, url, janeDoe);
+
+            //Assert
+            Assert.AreEqual(gift.Id, id);
+            Assert.AreEqual(gift.Title, title);
+            Assert.AreEqual(gift.Description, description);
+            Assert.AreEqual(gift.Url, url);
+            Assert.IsNotNull(gift.User);
+        }
+    }
+}
