0f0e4b94df3dc2475b352b955e54116f1c7f2b1a
Meg <mmwoodfo@asu.edu>
2020-01-16 11:12:13 -0700

    Refactoring Gift/User classes and adding new classes

---
+++ SecretSanta/src/SecretSanta.Data/Group.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    public class Group : FingerPrintEntityBase
+    {
+        public string Name { get => _Name; set => _Name = value ?? throw new ArgumentNullException(nameof(value)); }
+        private string _Name = string.Empty;
+    }
+}
