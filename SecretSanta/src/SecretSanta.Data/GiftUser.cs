32b2d7b22b7f3317750d5075f6633eb4151b0c8d
Meg <mmwoodfo@asu.edu>
2020-01-17 14:07:33 -0700

    Adding GiftUser class

---
+++ SecretSanta/src/SecretSanta.Data/GiftUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Data
+{
+    class GiftUser
+    {
+        public int GiftId { get; set; }
+        public Gift Gift { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
