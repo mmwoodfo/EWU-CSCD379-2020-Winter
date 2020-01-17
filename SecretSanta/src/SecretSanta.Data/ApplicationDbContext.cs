a65c8b87530402498ff1bfa7ab46341aa0466b3d
Meg <mmwoodfo@asu.edu>
2020-01-17 13:52:40 -0700

    Started work on ApplicationDbContext class

---
+++ SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Gift> Gifts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //TODO
+        }
+
+        public override int SaveChanges()
+        {
+            AddFingerPrinting();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+        {
+            AddFingerPrinting();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private void AddFingerPrinting()
+        {
+            //TODO
+        }
+    }
+}
