51147afcda98e695d4a5734bf18d558166aaa007
Meg <mmwoodfo@asu.edu>
2020-01-17 16:26:00 -0700

    Adding OnModelCreating method logic

--- SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
+++ SecretSanta/src/SecretSanta.Data/ApplicationDbContext.cs
@@ -1,26 +1,42 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System;
 
 namespace SecretSanta.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet<Gift>? Gifts { get; set; }
-        public DbSet<User>? Users { get; set; }
-        DbSet<UserGroup>? UserGroup { get; set; }
+        public DbSet<Gift> Gifts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserGroup> UserGroups { get; set; }
         public IHttpContextAccessor? HttpContextAccessor { get; set; }
 
+#nullable disable
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
         {
             HttpContextAccessor = httpContextAccessor;
         }
+#nullable enable
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            if (modelBuilder is null)
+                throw new ArgumentNullException(nameof(modelBuilder));
 
+            modelBuilder.Entity<UserGroup>().HasKey(ug => new { ug.UserId, ug.GroupId });
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(ug => ug.User)
+                .WithMany(u => u.UserGroups)
+                .HasForeignKey(ug => ug.UserId);
+
+            modelBuilder.Entity<UserGroup>()
+                .HasOne(ug => ug.Group)
+                .WithMany(g => g.UserGroups)
+                .HasForeignKey(ug => ug.GroupId);
         }
 
         public override int SaveChanges()
--- SecretSanta/src/SecretSanta.Data/UserGroup.cs
+++ SecretSanta/src/SecretSanta.Data/UserGroup.cs
@@ -6,9 +6,11 @@ namespace SecretSanta.Data
 {
     public class UserGroup
     {
+#nullable disable
         public int UserId { get; set; }
-        public User? User { get; set; }
+        public User User { get; set; }
         public int GroupId { get; set; }
-        public Group? Group { get; set; }
+        public Group Group { get; set; }
+#nullable enable
     }
 }
