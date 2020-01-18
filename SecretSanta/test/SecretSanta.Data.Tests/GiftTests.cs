85322326037739c1ce6f5f76da142aef3ab455ac
Meg <mmwoodfo@asu.edu>
2020-01-17 18:53:51 -0700

    Fixing some things

--- SecretSanta/test/SecretSanta.Data.Tests/GiftTests.cs
+++ SecretSanta/test/SecretSanta.Data.Tests/GiftTests.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SecretSanta.Data.Tests
 {
