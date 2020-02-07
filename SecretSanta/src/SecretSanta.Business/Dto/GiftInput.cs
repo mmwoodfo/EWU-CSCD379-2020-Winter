34dbeb8de0b27d3abe7732240b95acd70788c965
Meg <mmwoodfo@asu.edu>
2020-02-07 06:24:18 -0700

    Adding UserId

--- SecretSanta/src/SecretSanta.Business/Dto/GiftInput.cs
+++ SecretSanta/src/SecretSanta.Business/Dto/GiftInput.cs
@@ -5,6 +5,7 @@
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? Url { get; set; }
+        public int? UserId { get; set; }
         public User? User { get; set; }
     }
 }
