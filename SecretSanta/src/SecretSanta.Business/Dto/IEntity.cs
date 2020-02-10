80d9241cf26aaef1b67f026def84226a306556c7
Meg <mmwoodfo@asu.edu>
2020-02-09 20:44:27 -0700

    Code Cleanup

--- SecretSanta/src/SecretSanta.Business/Dto/IEntity.cs
+++ SecretSanta/src/SecretSanta.Business/Dto/IEntity.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SecretSanta.Business.Dto
+﻿namespace SecretSanta.Business.Dto
 {
     public interface IEntity
     {
